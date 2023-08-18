1270a480377ac6e504bdb2cb8b8a35ddb6b4714a
VijaiPatel <48962282+VijaiPatel@users.noreply.github.com>
2023-08-18 14:47:04 +0100

    Add project files.

---
+++ DependencyInjection/ICalculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjection
+{
+    internal interface ICalculator
+    {
+        public void Add(int a, int b);
+        public void Divide(int a, int b);
+    }
+}
---
+++ DependencyInjection/Ilog.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjection
+{
+    internal interface ILog
+    {
+        public void LogMess(string message);
+
+    }
+}
